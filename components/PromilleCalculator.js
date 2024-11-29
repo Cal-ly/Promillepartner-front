519d161baf896fa74f5648589d4a772755fb300b
MathiasNoergaard <man010@edu.zealand.dk>
2024-11-29 12:40:44 +0100

    Begyndt på story 3 med mere visuel druk plan
    Co-authored-by: CharlineBennekou <CharlineBennekou@users.noreply.github.com>
    Co-authored-by: NikolajKofoed <NikolajKofoed@users.noreply.github.com>

--- components/PromilleCalculator.js
+++ components/PromilleCalculator.js
@@ -29,9 +29,35 @@ export default {
         <p>
           Du skal drikke ca. <strong>{{ result }}</strong> genstand(e) i timen
           for at nå din ønskede promille.
+          forventet alkohol drukket {{ totalAlcohol }}gram
+
         </p>
       </div>
     </div>
+    <div v-if="result != null">
+      <select id="dropDownSelectDrinks" v-model="selectedDrinks" multiple>
+        <option  v-for="item in retrievedDrinks">{{ item.name }}</option>
+      </select>
+      <button v-on:click="addToDrinkList">Tilføj</button>
+
+      <!-- DISPLAY DRINKS IN DRINKING PLAN -->
+      <ul>
+        <li v-for="(item, index) in savedDrinks" :key="index">
+          {{ item.name }} 
+          {{ item.id }} 
+          <!-- DELETE METHOD FOR DRINKS IN DRINKING PLAN -->
+          <button v-on:click="removeFromDrinkList(item.id)">Delete</button>
+        </li>
+      </ul>
+
+      <p>{{forventedAlkohol}}</p>
+
+      
+      <!--<ul>
+            <li v-for="item in retrievedDrinks">{{ item.volume }} liter of {{item.name}} contains {{ item.volume * item.alcoholPercentOfVolume * 7,89 }} gram alcohol</li>
+          </ul>-->
+    </div>
+
   `,
   data() {
     return {
@@ -41,17 +67,69 @@ export default {
       targetPromille: 0.0,
       hours: 1,
       result: null,
+      totalAlcohol: 0,
+      retrievedDrinks: [],
+      loadedData: false,
+      selectedDrinks: [],
+      savedDrinks: [],
+      forventedAlkohol: 0,
     };
   },
   methods: {
+    async showAllDrinks() {
+      try {
+        const response = await axios.get("https://promillepartnerbackend.azurewebsites.net/api/drink");
+        this.retrievedDrinks = response.data;
+        return this.retrievedDrinks; // Return the fetched data
+      } catch (ex) {
+        alert(ex.message);
+      }
+    },
     calculate() {
       const alcoholDistributionFactor = this.gender === "male" ? 0.7 : 0.6; // Mand: 0.7, Kvinde: 0.6
       const standardDrinkAlcohol = 12; // Alkohol i gram per genstand
+      const nedbrydningsHastighed = 0.15;
+      console.log(this.selectedDrinks)
       const requiredAlcohol =
         this.targetPromille * this.weight * alcoholDistributionFactor +
-        0.15 * this.weight * this.hours;
+        nedbrydningsHastighed * this.weight * this.hours;
+      this.showAllDrinks();
+      this.totalAlcohol = requiredAlcohol;
       const drinksPerHour = requiredAlcohol / (standardDrinkAlcohol * this.hours);
       this.result = Math.max(0, drinksPerHour.toFixed(2)); // Ingen negative værdier
     },
+    addToDrinkList() {
+      this.selectedDrinks //selected drinks from select element
+      this.selectedDrinks.forEach(d => this.savedDrinks.push({name: d, id:this.savedDrinks.length }))
+      this.forventedAlkohol = 0
+
+      //simpel version, advancerede skal være man kan kun tilføje drinks indefor totalalcohol 
+      //+ hvis den overstiger ændre tiden også i drukplanen for at udligne
+      if (this.totalAlcohol > this.forventedAlkohol)
+        { 
+          for (let i = 0; i < this.savedDrinks.length; i++) 
+          {
+            let temp = this.retrievedDrinks.find(d => d.name == this.savedDrinks[i]);
+            console.log(temp)
+            this.forventedAlkohol += temp.volume * temp.alcoholPercentOfVolume * 7,89
+          } 
+        }
+
+        this.forventedAlkohol = this.forventedAlkohol.toFixed(2)
+
+        
+      // = this.savedDrinks.forEach(sD => this.retrievedDrinks.find(rD => rD.name == sD).volume)
+    },
+    removeFromDrinkList(index) {
+      this.savedDrinks.indexOf(index)
+      //this.savedDrinks = delete this.savedDrinks[index]
+      const index2 = this.savedDrinks.indexOf(sD => sD.id == index);
+      if (index2 > -1) { // only splice array when item is found
+        this.savedDrinks = this.savedDrinks.splice(index2 -1, 1); // 2nd parameter means remove one item only
+      }
+    },
   },
+  mounted() {
+    this.showAllDrinks();
+  }
 };
